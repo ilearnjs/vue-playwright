d1ed689f2d528f8e8df6b047d1490176fd3f64f8
ilearnjs <20753143+ilearnjs@users.noreply.github.com>
2025-09-30 18:15:19 +0200

    add gh actions

--- fe/playwright.config.ts
+++ fe/playwright.config.ts
@@ -6,34 +6,45 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on',
   },
+
+  outputDir: '.tests/test-results/',
+  snapshotPathTemplate: process.env.CI
+    ? '.tests/snaps/{testFilePath}/{arg}-{projectName}-{platform}{ext}'
+    : 'tests/snaps/{testFilePath}/{arg}-{projectName}-{platform}{ext}',
+  reporter: [
+    ['html', {
+      outputFolder: '.tests/test-report',
+      open: 'never',
+    }],
+    process.env.CI ? ['github'] : ['line'],
+  ],
 
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
   ],
   webServer: {
     command: 'yarn dev',
