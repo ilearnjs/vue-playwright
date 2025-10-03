17bbd276b43fcc1f93bfcd143f9876303d7e7140
ilearnjs <20753143+ilearnjs@users.noreply.github.com>
2025-10-03 13:31:54 +0200

    update ci

--- fe/playwright.config.ts
+++ fe/playwright.config.ts
@@ -70,8 +70,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
+  workers: undefined,
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on'
