adc938a8d720c3342e38133353e7a30015fd2054
ilearnjs <20753143+ilearnjs@users.noreply.github.com>
2025-10-16 14:22:00 +0200

    test

--- fe/tests/about.spec.ts
+++ fe/tests/about.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from './_setup'
 
 test.describe('About Page Visual Tests', () => {
-  test('init state', async ({ page }) => {
+  test('init state', async ({ page }, testInfo) => {
     await page.goto('/about')
     await page.waitForLoadState('networkidle')
 
     await expect(page).toHaveScreenshot('about-page.png', {
       fullPage: true,
       animations: 'disabled',
+    })
+
+    testInfo.attach('page.html', {
+      body: await page.content(),
+      contentType: 'text/html',
     })
   })
 })
