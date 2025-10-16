598fc7af7c866556bcdf1f9514d46b99ba584024
ilearnjs <20753143+ilearnjs@users.noreply.github.com>
2025-10-16 15:10:44 +0200

    Update playwright config (#27)
    * test
    
    * replace trace with attaching page state
    
    * update trace settings

--- fe/tests/_setup.ts
+++ fe/tests/_setup.ts
@@ -1,17 +1,33 @@
 import { test as base, expect } from '@playwright/test'
 
 type AuthFixture = {
-  auth: () => Promise<void>;
-};
+  auth: () => Promise<void>
+  addFinalPageState: void
+}
 
 export const test = base.extend<AuthFixture>({
   auth: async ({ context }, use) => {
     await use(async () => {
       await context.addCookies([
         { name: 'session_id', value: '1', path: '/', domain: 'localhost' }
-      ]);
+      ])
     })
   },
+
+  addFinalPageState: [async ({ page }, use, testInfo) => {
+    await use()
+
+    try {
+      if (!page.isClosed()) {
+        const content = await page.content()
+        await testInfo.attach(`test-end-${testInfo.title.replace(/\s+/g, '-')}.html`, {
+          body: content,
+          contentType: 'text/html'
+        })
+      }
+    } catch (e) {
+    }
+  }, { auto: true }]
 })
 
 export { expect }
